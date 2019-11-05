fae951d44b1a9aa746f87ca8514039f7fd4f718e
MFBouzon <murillobouzon@gmail.com>
2019-11-05 18:20:52 -0300

    Exercise 2 Updated

--- Exercicio 2/main.py
+++ Exercicio 2/main.py
@@ -12,9 +12,15 @@ Created on Thu Oct 31 21:07:31 2019
 import statistic
 import pandas as pd
 
-data = pd.read_csv("alpswater.csv", sep=';', header = None)
-data = data.values.tolist()
-
+data = pd.read_csv("data.csv", sep=';', header = None)
+data = data.values.T.tolist()
 print(data)
+covMatrix = statistic.getCovMatrix(data)
+print(covMatrix)
+eigenValues = statistic.getEigenValues(covMatrix)
+eigenVector = statistic.getEigenVector(covMatrix, eigenValues)
 
-print(statistic.getCovMatrix(data))
+print(eigenValues, "\n", eigenVector)
+
+data2 = [[2, 1], [1,2]]
+print(statistic.getEigenVector(data2, statistic.getEigenValues(data2)))
--- Exercicio 2/statistic.py
+++ Exercicio 2/statistic.py
@@ -8,6 +8,8 @@ Created on Thu Oct 31 21:07:31 2019
 @project: PEL-208 Exercicio 2: Implementação da Análise de Componentes Principal (PCA)
 
 """
+from math import sqrt
+import numpy as np
 
 def mean(data):
     return sum(data)/len(data)
@@ -24,3 +26,50 @@ def covariance(x, y):
 def getCovMatrix(data):
     cov = [[covariance(data[x],data[y]) for x in range(len(data))] for y in range(len(data))]
     return cov
+
+def bhaskara(a, b, c):
+    
+    delta = b**2 - (4*a*c)
+    if delta < 0:
+        return []
+    elif delta == 0:
+        return [(-b)/(2*a)]
+    else:
+         return [((-b)+sqrt(delta))/(2*a), ((-b)-sqrt(delta))/(2*a)]
+
+def gauss (a, b):
+  n = len(a)
+  for k in range(0, n-1, 1):
+    for i in range(k+1, n, 1):
+      l = a[i][k] / a[k][k]
+      for j in range(k, n, 1):
+        a[i][j] = a[i][j] - l * a[k][j]
+      b[i] = b[i] - l * b[k]
+  return(b, a)
+    
+def solveLin(mat, v):
+    n = len(mat)
+    vec = np.empty(n)
+    vec.fill(1)
+    for i in range(len(mat)):
+        mat[i][i] = mat[i][i] - v
+    if np.linalg.det(mat) != 0:  
+        ret = np.dot(vec, np.linalg.inv(mat))
+    return ret
+
+def getEigenValues(data):
+    
+    a = 1
+    b = -(data[0][0]+data[1][1])
+    c = (data[0][0]*data[1][1]) - (data[1][0]*data[0][1])
+    
+    print(a, b, c)
+    
+    return bhaskara(a, b, c)
+
+def getEigenVector(cov, eValues):
+    eVec = []
+    for i in eValues:
+        res = solveLin(cov, i)
+        eVec.append(res.tolist())
+    return eVec
